3a9c145e9fcc596dafdccb46b6626f484600bce1
ASUS <dnmonakhov2507@gmail.com>
2026-06-18 22:57:35 +0300

    Добавлены constants/colors.ts и исправлен импорт

---
+++ app/constants/colors.ts
@@ -0,0 +1,20 @@
+export default {
+  light: {
+    primary: '#007AFF',
+    secondary: '#5856D6',
+    background: '#FFFFFF',
+    card: '#F2F2F7',
+    text: '#000000',
+    border: '#E5E5EA',
+    notification: '#FF3B30',
+  },
+  dark: {
+    primary: '#0A84FF',
+    secondary: '#5E5CE6',
+    background: '#000000',
+    card: '#1C1C1E',
+    text: '#FFFFFF',
+    border: '#38383A',
+    notification: '#FF453A',
+  },
+};
